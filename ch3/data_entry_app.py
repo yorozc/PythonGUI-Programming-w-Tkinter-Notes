e50c7903fbf60dea187259a5746d35dd1d22afd3
yorozc <yorozcolop49@gmail.com>
2025-11-28 15:08:26 -0800

    added plant data section

--- ch3/data_entry_app.py
+++ ch3/data_entry_app.py
@@ -1,6 +1,7 @@
 # data_entry_app.py
 """The ABQ Data Entry application"""
 
+from pydoc import text
 import tkinter as tk 
 from tkinter import ttk
 from datetime import datetime
@@ -114,5 +115,54 @@ ttk.Checkbutton(
     e_info, variable=variables['Equipment Fault'], text='Equipment Fault'
 ).grid(row=2, column=0, sticky=(tk.W + tk.E))
 
+# Plant data section
+p_info = ttk.LabelFrame(drf, text='Plant Data')
+p_info.grid(sticky=(tk.W + tk.E))
+for i in range(3):
+    p_info.columnconfigure(i, weight=1)
+
+# Plants entry
+variables['Plants'] = tk.IntVar()
+ttk.Label(p_info, text='Plants').grid(row=0, column=0)
+ttk.Spinbox(
+    p_info, textvariable=variables['Plants'],
+    from_=0, to=20, increment=1
+).grid(row=1, column=0, sticky=(tk.W + tk.E))
+
+# Blossoms entry
+variables['Blossoms'] = tk.IntVar()
+ttk.Label(p_info, text='Blossoms').grid(row=0, column=1)
+ttk.Spinbox(
+    p_info, textvariable=variables['Blossoms'],
+    from_=0, to=1000, increment=1
+).grid(row=1, column=1, sticky=(tk.W + tk.E))
+
+variables['Fruit'] = tk.IntVar()
+ttk.Label(p_info, text='Fruit').grid(row=0, column=2)
+ttk.Spinbox(
+    p_info, textvariable=variables['Fruit'],
+    from_=0, to=1000, increment=1
+).grid(row=1, column=2, sticky=(tk.W + tk.E))
+
+variables['Min Height'] = tk.DoubleVar()
+ttk.Label(p_info, text='Min Height (cm)').grid(row=2, column=0)
+ttk.Spinbox(
+    p_info, textvariable=variables['Min Height'],
+    from_=0, to=1000, increment=0.01
+).grid(row=3, column=1, sticky=(tk.W + tk.E))
+
+variables['Max Height'] = tk.DoubleVar()
+ttk.Label(p_info, text='Max Height (cm)').grid(row=2, column=1)
+ttk.Spinbox(
+    p_info, textvariable=variables['Max Height'],
+    from_=0, to=1000, increment=0.01
+).grid(row=3, column=1, sticky=(tk.W + tk.E))
+
+variables['Med Height'] = tk.DoubleVar()
+ttk.Label(p_info, text='Median Height (cm)').grid(row=2, column=2)
+ttk.Spinbox(
+    p_info, textvariable=variables['Med Height'],
+    from_=0, to=1000, increment=0.01
+).grid(row=3, column=2, sticky=(tk.W + tk.E))
 
 root.mainloop()
