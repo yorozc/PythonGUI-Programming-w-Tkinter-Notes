1cff3f4211aa36908affa6ab185ebbd649497e37
yorozc <yorozcolop49@gmail.com>
2025-11-29 10:41:49 -0800

    on_reset func created

--- ch3/data_entry_app.py
+++ ch3/data_entry_app.py
@@ -182,6 +182,14 @@ ttk.Label(
     root, textvariable=status_variable
 ).grid(sticky=tk.W + tk.E, row=99, padx=10)
 
+def on_reset():
+    """Called when reset button is clicked, or after save"""
+    for variable in variables.values():
+        if isinstance(variable, tk.BooleanVar):
+            variable.set(False)
+        else:
+            variable.set('')
+    notes_inp.delete('1.0', tk.END)
 
 
 root.mainloop()
